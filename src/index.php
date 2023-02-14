a54f0176bc9c080e8dd0bd7d2296b90adf279515
mrfakename <me@mrfake.name>
2023-02-14 12:24:40 -0800

    Add autodelete

--- src/index.php
+++ src/index.php
@@ -4,14 +4,17 @@
 $conn = mysqli_connect('localhost', 'root', 'root', 'denote');
 $iv = "0000000000000000"; // string of numbers, length 16
 if (strlen($iv) !== 16) {
-  die("Sorry, your IV key length != 16");
+    die("Sorry, your IV key length != 16");
 }
+$delete_old_notes = true; # Auto-delete notes older than 30 days
 
 $base_url = 'https://base-api-url/denote?id='; // Path to index.php
 # End Config
 
 
-
+if ($delete_old_notes) {
+    $conn->query('DELETE FROM note WHERE datecreated < now() - INTERVAL 30 DAY');
+}
 
 $version = '0.4-beta';
 
