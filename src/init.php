65155c79e3215bd9c8dafd2a16b9f619984ff392
mrfakename <me@mrfake.name>
2023-02-14 12:26:52 -0800

    Create init file

---
+++ src/init.php
@@ -0,0 +1,11 @@
+<?php
+# Config
+$conn = mysqli_connect('localhost', 'root', 'root', 'denote');
+$iv = "0000000000000000"; // string of numbers, length 16
+if (strlen($iv) !== 16) {
+    die("Sorry, your IV key length != 16");
+}
+$delete_old_notes = true; # Auto-delete notes older than 30 days
+
+$base_url = 'https://base-api-url/denote?id='; // Path to index.php
+# End Config
